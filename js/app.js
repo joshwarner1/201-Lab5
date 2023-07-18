363ad4b826148cd89195a45d168e41650e63fdc6
Josh Warner <joshwarner315@gmail.com>
2023-07-17 21:37:57 -0500

    quest5

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
- testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -105,11 +105,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let multiplesArray = multiply(multArr[0], multiply(multArr[1], multArr[2])[0])[0];
+  let multiplesString = `The numbers ${multArr} have a product of ${multiplesArray}.`;
 
+  return [multiplesArray, multiplesString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
