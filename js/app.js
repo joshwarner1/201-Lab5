0a9dc2e61cc5545a0c0b923e240cae946849cd38
Josh Warner <joshwarner315@gmail.com>
2023-07-17 21:12:47 -0500

    quest3

--- js/app.js
+++ js/app.js
@@ -33,9 +33,9 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
   let product = a * b;
-  let prodString = `The product of ${a} and ${b} is ${product}.`
+  let prodString = `The product of ${a} and ${b} is ${product}.`;
 
-  return [product, prodString]
+  return [product, prodString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -55,12 +55,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { //eslint-disable-line 
+  let sumOnly = sum(a, sum(b, c)[0])[0];
+  let productOnly = multiply(a, multiply(b,c)[0])[0];
+  let sumString = `${a} and ${b} and ${c} sum to ${sumOnly}.`;
+  let productString = `The product of ${a} and ${b} and ${c} is ${productOnly}.`;
+  return [sumOnly, productOnly, sumString, productString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
